8f76d975eef5224f884765e2e98d6140e81dfca9
Szen <szenprogs@gmail.com>
2022-11-04 08:37:23 +0300

    Update sensor.py

--- custom_components/elehant_water/sensor.py
+++ custom_components/elehant_water/sensor.py
@@ -39,7 +39,7 @@ def update_counters(call):
             return
 
         # _LOGGER.debug("Found MAC = %s", str(mac))
-        """СГБТ-1.8, СГБТ-3.2, СГБТ-4.0"""
+        """СГБТ-1.8, СГБТ-3.2, СГБТ-4.0, СГБТ-4.0 ТК"""
         if (str(mac).find('b0:10:01') !=-1) or (str(mac).find('b0:11:01') !=-1) or (str(mac).find('b0:12:01') !=-1 or (str(mac).find('b0:32:01') !=-1)):
             _LOGGER.debug("SEE gaz counter")
             manufacturer_data = ev.retrieve("Manufacturer Specific Data")
